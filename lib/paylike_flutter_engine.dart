11650dd211302504904ecc5a5c9c1f71f3ad1189
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2022-05-26 15:49:38 +0200

    refactor(export): now sohuld export API_MODE

--- lib/paylike_flutter_engine.dart
+++ lib/paylike_flutter_engine.dart
@@ -1,6 +1,7 @@
 /// Contains the main business logic for the library
 library paylike_flutter_engine;
 
+export 'src/config/api.dart';
 export 'src/engine.dart';
 export 'src/engine_widget.dart';
 export 'package:paylike_currencies/paylike_currencies.dart';
